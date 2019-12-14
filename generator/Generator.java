fe00c6e3bd9efaae14117cfbb754f93ca7be23a6
杨丰畅 <ayang818@qq.com>
2019-12-15 00:08:27 +0800

    init project

---
+++ generator/Generator.java
@@ -0,0 +1,14 @@
+package generator;
+
+import java.util.UUID;
+
+/**
+ * @ClassName generator.Generator
+ * @Description 分布式唯一Id生成器
+ * @Author 杨丰畅
+ * @Date 2019/12/14 21:20
+ **/
+public class Generator {
+    public static void main(String[] args) {
+    }
+}
